4c7bf309ea9e7756493bfe33807d930fe52de824
Hellobraincode <hellobraincode@gmail.com>
2025-08-14 08:23:51 +0000

    nice одоо энэ project ийг ашигла // Import the functions you need from t

---
+++ src/lib/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQCVnBWzWYxtcahv_y4DeRtFxSuxJlAJk",
+  authDomain: "kino-web-7d98c.firebaseapp.com",
+  projectId: "kino-web-7d98c",
+  storageBucket: "kino-web-7d98c.appspot.com",
+  messagingSenderId: "350705885395",
+  appId: "1:350705885395:web:d8bbaa96993dd2ee457426"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
